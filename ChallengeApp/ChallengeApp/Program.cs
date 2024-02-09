3c21acfe2e9cbe26e83eacb8fee3ba411f283005
KrzysztofSurala <krzysztof@grafiks.pl>
2024-02-09 01:29:19 +0100

    Zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,77 @@
-﻿var name = "Ewa";
-var sex = "K";
-var age = 60;
+﻿
+// wyświetlanie liczby
+int numbers = 1060;
+// zamiana liczby na stringa
+string numersAsString = numbers.ToString();
+// string zrobi tablicę czaru
+char[] letters = numersAsString.ToArray();
 
-if (age < 30 && sex == "K")
+Console.WriteLine("Number: "); Console.WriteLine(letters);
+
+int zero = 0;
+int one = 0;
+int two = 0;
+int three = 0;
+int four = 0;
+int five = 0;
+int six = 0;
+int seven = 0;
+int eight = 0;
+int nine = 0;
+
+
+foreach (char number in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (number == '0')
+    {
+        zero++;
+    }
+    else if (number == '1')
+    {
+        one++;
+    }
+    else if (number == '2')
+    {
+        two++;
+    }
+    else if (number == '3')
+    {
+        three++;
+    }
+    else if (number == '4')
+    {
+        four++;
+    }
+    else if (number == '5')
+    {
+        five++;
+    }
+    else if (number == '6')
+    {
+        six++;
+    }
+    else if (number == '7')
+    {
+        seven++;
+    }
+    else if (number == '8')
+    {
+        eight++;
+    }
+    else if (number == '9')
+    {
+        nine++;
+    }
+    ;
 }
-else if (age == 30 && name == "Ewa")
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (age < 18 && sex == "M")
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
+Console.WriteLine("0 => " + zero);
+Console.WriteLine("1 => " + one);
+Console.WriteLine("2 => " + two);
+Console.WriteLine("3 => " + three);
+Console.WriteLine("4 => " + four);
+Console.WriteLine("5 => " + five);
+Console.WriteLine("6 => " + six);
+Console.WriteLine("7 => " + seven);
+Console.WriteLine("8 => " + eight);
+Console.WriteLine("9 => " + nine);
